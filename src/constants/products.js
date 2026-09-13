409bbe72ecce005e7d6eea5fce0a77f68014755d
imsrpanda <43740337+imsrpanda@users.noreply.github.com>
2026-09-13 15:59:10 +0530

    Update HSN codes and add new products

--- src/constants/products.js
+++ src/constants/products.js
@@ -1,12 +1,14 @@
 export const PRODUCTS = [
-    { name: 'Intecium-XT TABLET', hsn: '210690', pack: '1x10' },
+    { name: 'Intecium-XT TABLET', hsn: '21061000', pack: '1x10' },
     { name: 'INTE-WELL SYRUP', hsn: '21061000', pack: '1x1' },
     { name: 'INTE-ZYME SYRUP', hsn: '21061000', pack: '1x1' },
     { name: 'Intefert-f', hsn: '21069099', pack: '1x10' },
     { name: 'Intefert-d', hsn: '21069099', pack: '1x10' },
     { name: 'INTE-WELL DROP', hsn: '21061000', pack: '1x1' },
     { name: 'INTEZYME DROP', hsn: '21061000', pack: '1x1' },
-    { name: 'INTE-WELL TABLET', hsn: '21061000', pack: '1x10' }
+    { name: 'INTE-WELL TABLET', hsn: '21061000', pack: '1x10' },
+    { name: 'INTE-ZYME DROP', hsn: '21061000', pack: '1x1' },
+    { name: 'INTECIUM-MG', hsn: '21061000', pack: '1x10' }
 ];
 
 export const PRODUCT_NAMES = PRODUCTS.map(p => p.name);
